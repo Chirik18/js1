728c7a162e6e3805918da0a75a9a555cf35c5af0
Chirik18 <cherniavskyi0729@gmail.com>
2025-08-14 19:34:21 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// let name = "Іван";
+// let city = "Kyiv";
+// city = name;
+//console.log(city);
+
+// let name = "Sasha";
+// console.log(`привіт ${1}`); // привіт 1
+// console.log(`привіт ${"name"}`); // привіт name
+// console.log(`привіт ${name}`); // привіт Sasha
+
+// let a = "5";
+// let b = "13cvb";
+// let c = "12.9sxdcfgv";
+// console.log(typeof Number(a));
+// console.log(typeof parseInt(b));
+// console.log(typeof parseFloat(c));
+
+//  console.log((0.1 * 10 + 0.2 * 10) / 10);
+
+// console.log(Math.max(20, 10, 50, 40));
+
+// console.log(Math.random() * (4 -2) + 2);
+
+// const message = "Welcome to Bahamas!";
+// console.log(message.length);  //19
+
+// const message = "Welcome to Bahamas!";
+// console.log(message.toUpperCase());
+
+// let user = {};
+// user.name = "Sasha";
+// user.age = 18;
+// user.city = "Kyiv";
+// console.log(user);
+// delete user.city;
+// user['like flowers'] = true;
+// console.log(user);
+
+// for (key in user) {
+//    console.log(key);
+//    console.log(user[key]);
+// }
